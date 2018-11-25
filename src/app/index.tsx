ed9d1e8774ddc16992d2552313d5a7c104592d43
Sergey Nechaev <snechayev@gmail.com>
2018-11-26 01:06:13 +0300

    Material UI and icons added

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,9 +2,18 @@ import * as React from 'react';
 import { Route, Switch } from 'react-router';
 import { Editor } from 'app/containers/editor';
 import { hot } from 'react-hot-loader';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  typography: {
+    useNextVariants: true,
+  },
+});
 
 export const App = hot(module)(() => (
-  <Switch>
-    <Route path="/" component={Editor} />
-  </Switch>
+  <MuiThemeProvider theme={theme}>
+    <Switch>
+      <Route path="/" component={Editor} />
+    </Switch>
+  </MuiThemeProvider>
 ));
